4d291b96bf5f4b921cc3b3554fb0eb357cdda58f
theteslascope <teslascope1990@gmail.com>
2021-02-24 12:12:22 -0300

    CP commiteado por theteslascope a las 1614179541863

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -51,8 +51,13 @@ function map(array, cb) {
   // Itera sobre cada valor en "array", pásalo a `cb` y luego ubicar el valor devuelto por `cb` en un nuevo array
   // El nuevo array debe tener la misma longitud que el array del argumento
   //Tu código:
-  var array2 = [];
+
+  //var nuevoArray = [];
   
+  var nuevoArray = array.map(function(x) {
+    return cb(x)
+ });
+  return nuevoArray;
 
 }
 
